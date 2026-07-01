911490c5fbe5353c151604468919e9db9a38f6dc
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-07-01 15:08:00 +0200

    bump-web-instance-gateway

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation("no.novari:fint-arkiv-resource-model-java:$fintResourceModelVersion")
     implementation("no.novari:fint-administrasjon-resource-model-java:$fintResourceModelVersion")
 
-    implementation("no.novari:flyt-web-instance-gateway:2.0.2")
+    implementation("no.novari:flyt-web-instance-gateway:2.3.0")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-core")
